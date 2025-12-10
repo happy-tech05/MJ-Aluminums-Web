32199b610fa733b6a6f762cb6846416049c4687b
happy-tech05 <ramaphokohappy@gmail.com>
2025-12-10 10:38:55 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
 const menuToggle = document.getElementById('menu-toggle');
 const navLinks = document.getElementById('nav-links');
 
-// Open/close when clicking the hamburger
+
 menuToggle.addEventListener('click', (e) => {
-  e.stopPropagation();              // stop click from bubbling
+  e.stopPropagation();              
   navLinks.classList.toggle('active');
 });
 
-// Close when clicking outside
+
 document.addEventListener('click', (e) => {
-  // If menu is open AND click is NOT inside navLinks AND not on toggle button
+  
   if (
     navLinks.classList.contains('active') &&
     !navLinks.contains(e.target) &&
@@ -19,7 +19,7 @@ document.addEventListener('click', (e) => {
   }
 });
 
-// Prevent closing when clicking inside the navbar
+
 navLinks.addEventListener('click', (e) => {
   e.stopPropagation();
 });
